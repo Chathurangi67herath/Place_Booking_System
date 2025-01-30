b22516def87d56b0e32876332dfd131bef540ac9
Chathurangi67herath <73054107+Chathurangi67herath@users.noreply.github.com>
2025-01-30 22:06:38 +0530

    CREATED => Dashboard page with main navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider, theme } from "@chakra-ui/react";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+import BookingBody from "./Components/BookingBody";
+import Contacts from "./Components/Contacts";
+import DashboardBody from "./Components/DashboardBody";
+import RecentBookings from "./Components/RecentBookings";
+import CommonPage from "./pages/CommonPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={theme}>
+      <Router>
+        <CommonPage></CommonPage>
+        <Routes>
+          <Route exact path="/" element={<DashboardBody />} />
+          <Route exact path="/hallbooking" element={<BookingBody />} />
+          <Route exact path="/contacts" element={<Contacts />} />
+          <Route exact path="/recentBookings" element={<RecentBookings />} />
+        </Routes>
+      </Router>
+    </ChakraProvider>
   );
 }
 
